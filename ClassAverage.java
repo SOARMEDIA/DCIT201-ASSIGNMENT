249d95ab31360170d521074b7a4c44485c0b6a28
RedeemerW <rjkwilliams@st.ug.edu.gh>
2026-02-06 00:49:21 +0000

    wel

--- ClassAverage.java
+++ ClassAverage.java
@@ -1,5 +1,5 @@
 public static double calculateClassAverage(double score1, double score2) {
-    return (score1 + score2) / 2
+    return (score1 + score2) / 2;
 }
 
 public static double calculateClassAverage(double score1, double score2, double score3) {
